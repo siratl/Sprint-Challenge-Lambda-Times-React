4c0f81c166486e69f3c2349d987dd162f187864f
siratl <elishubax@gmail.com>
2019-01-18 10:04:11 -0800

    Validated passed props using PropTypes

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,19 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Card = props => {
   return (
     <div className="card">
-      <div className="headline">{/* headline goes here */props.card.headline}</div>
+      <div className="headline">
+        {/* headline goes here */ props.card.headline}
+      </div>
       <div className="author">
         <div className="img-container">
-          <img alt='Displayed Card' src={props.card.img /* image source goes here */} />
+          <img
+            alt="Displayed Card"
+            src={props.card.img /* image source goes here */}
+          />
         </div>
-        <span>By {/* author goes here */props.card.author}</span>
+        <span>By {/* author goes here */ props.card.author}</span>
       </div>
     </div>
   );
 };
 
 // Make sure to include PropTypes.
+Card.propTypes = {
+  card: PropTypes.shape({
+    headline: PropTypes.string,
+    author: PropTypes.string,
+    img: PropTypes.string,
+  }),
+};
 
 export default Card;
